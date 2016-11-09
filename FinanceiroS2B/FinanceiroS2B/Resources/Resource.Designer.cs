11ab977f84c0d1ea227c99115d94e83510b93160
Pedro Marcus Paixão Silva <pedromarcussilva38@gmail.com>
2016-11-09 15:57:23 -0200

    Nova tela criada, tela de saldo.

--- FinanceiroS2B/FinanceiroS2B/Resources/Resource.Designer.cs
+++ FinanceiroS2B/FinanceiroS2B/Resources/Resource.Designer.cs
@@ -77,7 +77,10 @@ namespace FinanceiroS2B
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Conta = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
